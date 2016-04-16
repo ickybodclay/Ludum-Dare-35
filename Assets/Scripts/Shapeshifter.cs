d309b628ae71260e1d22d1830c3883fd1145b43f
Jason Petterson <jp@brokenshotgun.com>
2016-04-16 16:15:22 -0400

    fixed shapeshifter character sheet by adding padding

--- Assets/Scripts/Shapeshifter.cs
+++ Assets/Scripts/Shapeshifter.cs
@@ -1,24 +1,36 @@
 ﻿using UnityEngine;
 
 public class Shapeshifter : MonoBehaviour {
+    public enum Form {
+        HUMAN = 0,
+        BIRD,
+        BEAR,
+        FOX
+    }
+
     private Animator m_Animator;
+    private Form m_CurrentForm;
 
 	void Start () {
         m_Animator = GetComponent<Animator>();
+
+        m_CurrentForm = Form.HUMAN;
 	}
 	
 	void Update () {
 	    if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            m_Animator.SetInteger("Form", 0);
+            m_CurrentForm = Form.HUMAN;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            m_Animator.SetInteger("Form", 1);
+            m_CurrentForm = Form.BIRD;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            m_Animator.SetInteger("Form", 2);
+            m_CurrentForm = Form.BEAR;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4)) {
-            m_Animator.SetInteger("Form", 3);
+            m_CurrentForm = Form.FOX;
         }
+
+        m_Animator.SetInteger("Form", (int)m_CurrentForm);
     }
 }
